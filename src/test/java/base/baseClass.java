d38cd89a3749ccad09270fbfb828a8ef4d1ee6e8
Soundarya <soundaryasuryawanshi1999@gmail.com>
2025-04-23 07:16:21 +0530

    parallel execution

--- src/test/java/base/baseClass.java
+++ src/test/java/base/baseClass.java
@@ -1,6 +1,7 @@
 package base;
 
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
@@ -8,89 +9,91 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.ITestResult;
-
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
-
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class baseClass {
 
-
-	private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-	private static ExtentReports extent;
-
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+    private static ExtentReports extent;
     protected static ThreadLocal<ExtentTest> test = new ThreadLocal<>();
-	 @BeforeSuite
-	    public void reportSetup() {
-		 if (extent == null)
-		 {
-		 ExtentSparkReporter htmlReporter = new ExtentSparkReporter("test-output/ExtentReport.html");
-		    extent = new ExtentReports();
-		    extent.attachReporter(htmlReporter);
-	    }
-}
 
-	   @BeforeMethod
-		// TODO Auto-generated method stub
-		public void setup(Method method)
-		{
-		   ChromeOptions options = new ChromeOptions();
-		   options.addArguments("--disable-save-password-bubble");
-		   options.setExperimentalOption("prefs", Map.of(
-		       "credentials_enable_service", false,
-		       "profile.password_manager_enabled", false
-		   ));
-		WebDriverManager.chromedriver().setup();
-		driver.set(new ChromeDriver());
-		getDriver().get("https://www.saucedemo.com/");
-		getDriver().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	    getDriver().manage().window().maximize();
+    @BeforeSuite
+    public void reportSetup() {
+        if (extent == null) {
+            ExtentSparkReporter htmlReporter = new ExtentSparkReporter("test-output/ExtentReport.html");
+            extent = new ExtentReports();
+            extent.attachReporter(htmlReporter);
+        }
+    }
 
-	    driver.set(getDriver());
-	    if (extent != null) {
-        ExtentTest extentTest = extent.createTest(method.getName());
-        test.set(extentTest);
-	    }
-        else {
+    @BeforeMethod
+    public void setup(Method method) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);
+        prefs.put("profile.password_manager_enabled", false);
+
+        options.setExperimentalOption("prefs", prefs);
+        options.addArguments("--disable-save-password-bubble");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-infobars");
+        options.addArguments("--start-maximized");
+        options.addArguments("--incognito");
+
+        WebDriver localDriver = new ChromeDriver(options);
+        driver.set(localDriver);
+        localDriver.get("https://www.saucedemo.com/");
+        localDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        localDriver.manage().window().maximize();
+
+        driver.set(localDriver);
+
+        if (extent != null) {
+            ExtentTest extentTest = extent.createTest(method.getName());
+            test.set(extentTest);
+        } else {
             throw new IllegalStateException("ExtentReports not initialized");
         }
-	}
-	   @AfterMethod
-		public void teardown(ITestResult result)
-		{
+    }
 
-		   if (result.getStatus() == ITestResult.FAILURE) {
-	            test.get().fail(result.getThrowable());
-	        } else if (result.getStatus() == ITestResult.SUCCESS) {
-	        	test.get().pass("Test Passed");
-	        }
-		   if (getDriver() != null) {
-			   getDriver().quit();
-	           driver.remove();
-		   }
-		   test.remove();
-	        }
+    @AfterMethod
+    public void teardown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            test.get().fail(result.getThrowable());
+        } else if (result.getStatus() == ITestResult.SUCCESS) {
+            test.get().pass("Test Passed");
+        }
 
+        if (getDriver() != null) {
+            getDriver().quit();
+            driver.remove();
+        }
 
-@AfterSuite
-public void reportTearDown() {
-    extent.flush();
-}
-public static WebDriver getDriver() {
-    return driver.get();
-}
+        test.remove();
+    }
 
-public ExtentTest getTest() {
-    return test.get();
-}
+    @AfterSuite
+    public void reportTearDown() {
+        extent.flush();
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public ExtentTest getTest() {
+        return test.get();
+    }
 }
